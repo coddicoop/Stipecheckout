287a239ddec8a914f81e587fc865645ba72d9468
coddicoop <165064241+coddicoop@users.noreply.github.com>
2025-04-28 12:36:48 -0400

    Update server.js
    updated line change 27

--- server.js
+++ server.js
@@ -20,7 +20,29 @@ app.get('/', (req, res) => {
 
 app.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
+    ui_mode: 'embedded',
     line_items: [
       {
-        price: 'si_SD451a3HdE5VjD', // Updated with your actual Price ID here
+        price: 'si_SD451a3HdE5VjD', // <-- double-check this is your Subscription Price ID
         quantity: 1,
+      },
+    ],
+    mode: 'subscription',
+    return_url: `${YOUR_DOMAIN}/success-html/?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${YOUR_DOMAIN}/cancel-html/`,
+  });
+
+  res.send({ clientSecret: session.client_secret });
+});
+
+app.get('/session-status', async (req, res) => {
+  const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
+
+  res.send({
+    status: session.status,
+    customer_email: session.customer_details.email,
+  });
+});
+
+const PORT = process.env.PORT || 4242;
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
